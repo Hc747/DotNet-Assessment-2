4fed946986c4fd3ce5200adb302329f60e167ea0
Hc747 <harrisoncole05@gmail.com>
2017-10-07 18:34:01 +1100

    UserListScreen logic UserModel

---
+++ Assessment2/Solution/MVCO/Model/UserModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Assessment2.Solution.MVCO.Model {
+
+    public class UserModel {
+
+        private readonly string _info;
+        private readonly double _average;
+        private readonly int _count;
+
+        public UserModel(string info, double average, int count) {
+            _info = info;
+            _average = average;
+            _count = count;
+        }
+
+        public string Info => _info;
+
+        public double Average => _average;
+
+        public int Count => _count;
+    }
+
+}
