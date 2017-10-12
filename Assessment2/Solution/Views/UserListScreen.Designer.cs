763e03edc716ed31340870490e029eb4e64e5616
Hc747 <harrisoncole05@gmail.com>
2017-10-12 16:32:47 +1100

    Program logic

--- Assessment2/Solution/Views/UserListScreen.Designer.cs
+++ Assessment2/Solution/Views/UserListScreen.Designer.cs
@@ -214,6 +214,7 @@ namespace Assessment2.Solution.Views
             this.data_grid.TabIndex = 0;
             this.data_grid.UseCustomBackColor = true;
             this.data_grid.UseCustomForeColor = true;
+            this.data_grid.AutoGenerateColumns = false;
             // 
             // guest_info_column
             // 
