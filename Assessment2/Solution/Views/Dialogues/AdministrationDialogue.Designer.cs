26a329692f3580e425dc7213ffeb59e747626f9e
Harrison Cole <12962712@student.uts.edu.au>
2017-10-15 17:40:12 +1100

    administration dialogue

--- Assessment2/Solution/Views/Dialogues/AdministrationDialogue.Designer.cs
+++ Assessment2/Solution/Views/Dialogues/AdministrationDialogue.Designer.cs
@@ -30,20 +30,15 @@
         {
             System.Windows.Forms.TableLayoutPanel login_container;
             MetroFramework.Controls.MetroLabel login_label;
-            System.Windows.Forms.TableLayoutPanel textfield_container;
             System.Windows.Forms.TableLayoutPanel register_container;
-            MetroFramework.Controls.MetroLabel register_label;
-            this.login_button = new MetroFramework.Controls.MetroButton();
-            this.username = new MetroFramework.Controls.MetroTextBox();
-            this.password = new MetroFramework.Controls.MetroTextBox();
-            this.register_button = new MetroFramework.Controls.MetroButton();
+            this.admin_values = new MetroFramework.Controls.MetroComboBox();
+            this.submit_button = new MetroFramework.Controls.MetroButton();
+            this.cancel_button = new MetroFramework.Controls.MetroButton();
+            this.info_label = new MetroFramework.Controls.MetroLabel();
             login_container = new System.Windows.Forms.TableLayoutPanel();
             login_label = new MetroFramework.Controls.MetroLabel();
-            textfield_container = new System.Windows.Forms.TableLayoutPanel();
             register_container = new System.Windows.Forms.TableLayoutPanel();
-            register_label = new MetroFramework.Controls.MetroLabel();
             login_container.SuspendLayout();
-            textfield_container.SuspendLayout();
             register_container.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,10 +46,10 @@
             // 
             login_container.ColumnCount = 1;
             login_container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            login_container.Controls.Add(this.login_button, 0, 2);
+            login_container.Controls.Add(this.info_label, 0, 2);
             login_container.Controls.Add(login_label, 0, 0);
-            login_container.Controls.Add(textfield_container, 0, 1);
             login_container.Controls.Add(register_container, 0, 3);
+            login_container.Controls.Add(this.admin_values, 0, 1);
             login_container.Location = new System.Drawing.Point(0, 31);
             login_container.Name = "login_container";
             login_container.RowCount = 4;
@@ -62,131 +57,44 @@
             login_container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
             login_container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
             login_container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            login_container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             login_container.Size = new System.Drawing.Size(240, 309);
             login_container.TabIndex = 5;
             // 
-            // login_button
+            // admin_values
             // 
-            this.login_button.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.login_button.Location = new System.Drawing.Point(82, 187);
-            this.login_button.Name = "login_button";
-            this.login_button.Size = new System.Drawing.Size(75, 23);
-            this.login_button.TabIndex = 2;
-            this.login_button.Text = "Login";
-            this.login_button.UseCustomBackColor = true;
-            this.login_button.UseCustomForeColor = true;
-            this.login_button.UseSelectable = true;
-            this.login_button.Click += new System.EventHandler(this.login_button_Click);
+            this.admin_values.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.admin_values.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.admin_values.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(181)))), ((int)(((byte)(254)))));
+            this.admin_values.FormattingEnabled = true;
+            this.admin_values.ItemHeight = 23;
+            this.admin_values.Location = new System.Drawing.Point(59, 92);
+            this.admin_values.Name = "admin_values";
+            this.admin_values.Size = new System.Drawing.Size(121, 29);
+            this.admin_values.TabIndex = 0;
+            this.admin_values.UseCustomBackColor = true;
+            this.admin_values.UseCustomForeColor = true;
+            this.admin_values.UseSelectable = true;
             // 
             // login_label
             // 
             login_label.Anchor = System.Windows.Forms.AnchorStyles.None;
             login_label.AutoSize = true;
-            login_label.Location = new System.Drawing.Point(38, 21);
+            login_label.Location = new System.Drawing.Point(45, 21);
             login_label.Name = "login_label";
-            login_label.Size = new System.Drawing.Size(163, 19);
+            login_label.Size = new System.Drawing.Size(150, 19);
             login_label.TabIndex = 5;
-            login_label.Text = "Already have an acccount?";
+            login_label.Text = "Administration Dialogue";
             login_label.UseCustomBackColor = true;
             login_label.UseCustomForeColor = true;
-            // 
-            // textfield_container
-            // 
-            textfield_container.ColumnCount = 1;
-            textfield_container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            textfield_container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            textfield_container.Controls.Add(this.username, 0, 0);
-            textfield_container.Controls.Add(this.password, 0, 1);
-            textfield_container.Location = new System.Drawing.Point(3, 64);
-            textfield_container.Name = "textfield_container";
-            textfield_container.RowCount = 2;
-            textfield_container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            textfield_container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            textfield_container.Size = new System.Drawing.Size(234, 86);
-            textfield_container.TabIndex = 1;
-            // 
-            // username
-            // 
-            this.username.Anchor = System.Windows.Forms.AnchorStyles.None;
-            // 
-            // 
-            // 
-            this.username.CustomButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.username.CustomButton.Image = null;
-            this.username.CustomButton.Location = new System.Drawing.Point(142, 1);
-            this.username.CustomButton.Name = "";
-            this.username.CustomButton.Size = new System.Drawing.Size(21, 21);
-            this.username.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
-            this.username.CustomButton.TabIndex = 1;
-            this.username.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
-            this.username.CustomButton.UseSelectable = true;
-            this.username.CustomButton.Visible = false;
-            this.username.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(181)))), ((int)(((byte)(254)))));
-            this.username.Lines = new string[0];
-            this.username.Location = new System.Drawing.Point(35, 10);
-            this.username.MaxLength = 32767;
-            this.username.Name = "username";
-            this.username.PasswordChar = '\0';
-            this.username.PromptText = "Username";
-            this.username.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.username.SelectedText = "";
-            this.username.SelectionLength = 0;
-            this.username.SelectionStart = 0;
-            this.username.ShortcutsEnabled = true;
-            this.username.Size = new System.Drawing.Size(164, 23);
-            this.username.TabIndex = 1;
-            this.username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.username.UseCustomBackColor = true;
-            this.username.UseCustomForeColor = true;
-            this.username.UseSelectable = true;
-            this.username.WaterMark = "Username";
-            this.username.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            this.username.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            // 
-            // password
-            // 
-            this.password.Anchor = System.Windows.Forms.AnchorStyles.None;
-            // 
-            // 
-            // 
-            this.password.CustomButton.Image = null;
-            this.password.CustomButton.Location = new System.Drawing.Point(142, 1);
-            this.password.CustomButton.Name = "";
-            this.password.CustomButton.Size = new System.Drawing.Size(21, 21);
-            this.password.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
-            this.password.CustomButton.TabIndex = 1;
-            this.password.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
-            this.password.CustomButton.UseSelectable = true;
-            this.password.CustomButton.Visible = false;
-            this.password.Lines = new string[0];
-            this.password.Location = new System.Drawing.Point(35, 53);
-            this.password.MaxLength = 32767;
-            this.password.Name = "password";
-            this.password.PasswordChar = '●';
-            this.password.PromptText = "Password";
-            this.password.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.password.SelectedText = "";
-            this.password.SelectionLength = 0;
-            this.password.SelectionStart = 0;
-            this.password.ShortcutsEnabled = true;
-            this.password.Size = new System.Drawing.Size(164, 23);
-            this.password.TabIndex = 2;
-            this.password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.password.UseCustomBackColor = true;
-            this.password.UseCustomForeColor = true;
-            this.password.UseSelectable = true;
-            this.password.UseSystemPasswordChar = true;
-            this.password.WaterMark = "Password";
-            this.password.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
-            this.password.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             // 
             // register_container
             // 
             register_container.ColumnCount = 2;
-            register_container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
-            register_container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
-            register_container.Controls.Add(this.register_button, 0, 0);
-            register_container.Controls.Add(register_label, 0, 0);
+            register_container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            register_container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            register_container.Controls.Add(this.submit_button, 1, 0);
+            register_container.Controls.Add(this.cancel_button, 0, 0);
             register_container.Location = new System.Drawing.Point(3, 248);
             register_container.Name = "register_container";
             register_container.RowCount = 1;
@@ -194,33 +102,46 @@
             register_container.Size = new System.Drawing.Size(234, 58);
             register_container.TabIndex = 6;
             // 
-            // register_button
+            // submit_button
             // 
-            this.register_button.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.register_button.Location = new System.Drawing.Point(154, 17);
-            this.register_button.Name = "register_button";
-            this.register_button.Size = new System.Drawing.Size(65, 23);
-            this.register_button.TabIndex = 3;
-            this.register_button.Text = "Register";
-            this.register_button.UseCustomBackColor = true;
-            this.register_button.UseCustomForeColor = true;
-            this.register_button.UseSelectable = true;
-            this.register_button.Click += new System.EventHandler(this.register_button_Click);
+            this.submit_button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.submit_button.Location = new System.Drawing.Point(138, 17);
+            this.submit_button.Name = "submit_button";
+            this.submit_button.Size = new System.Drawing.Size(75, 23);
+            this.submit_button.TabIndex = 2;
+            this.submit_button.Text = "Submit";
+            this.submit_button.UseCustomBackColor = true;
+            this.submit_button.UseCustomForeColor = true;
+            this.submit_button.UseSelectable = true;
+            this.submit_button.Click += new System.EventHandler(this.login_button_Click);
             // 
-            // register_label
+            // cancel_button
             // 
-            register_label.Anchor = System.Windows.Forms.AnchorStyles.None;
-            register_label.AutoSize = true;
-            register_label.FontSize = MetroFramework.MetroLabelSize.Small;
-            register_label.Location = new System.Drawing.Point(9, 21);
-            register_label.Name = "register_label";
-            register_label.Size = new System.Drawing.Size(122, 15);
-            register_label.TabIndex = 6;
-            register_label.Text = "Don\'t have an account?";
-            register_label.UseCustomBackColor = true;
-            register_label.UseCustomForeColor = true;
+            this.cancel_button.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cancel_button.Location = new System.Drawing.Point(26, 17);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(65, 23);
+            this.cancel_button.TabIndex = 3;
+            this.cancel_button.Text = "Cancel";
+            this.cancel_button.UseCustomBackColor = true;
+            this.cancel_button.UseCustomForeColor = true;
+            this.cancel_button.UseSelectable = true;
+            this.cancel_button.Click += new System.EventHandler(this.register_button_Click);
             // 
-            // LoginScreen
+            // info_label
+            // 
+            this.info_label.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.info_label.AutoSize = true;
+            this.info_label.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.info_label.Location = new System.Drawing.Point(69, 191);
+            this.info_label.Name = "info_label";
+            this.info_label.Size = new System.Drawing.Size(101, 15);
+            this.info_label.TabIndex = 7;
+            this.info_label.Text = "INFO_LABEL_TEXT";
+            this.info_label.UseCustomBackColor = true;
+            this.info_label.UseCustomForeColor = true;
+            // 
+            // AdministrationDialogue
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -230,25 +151,23 @@
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(240, 340);
             this.MinimumSize = new System.Drawing.Size(240, 340);
-            this.Name = "LoginScreen";
+            this.Name = "AdministrationDialogue";
             this.Opacity = 0.8D;
             this.Resizable = false;
             this.ShowIcon = false;
             this.Theme = MetroFramework.MetroThemeStyle.Dark;
             login_container.ResumeLayout(false);
             login_container.PerformLayout();
-            textfield_container.ResumeLayout(false);
             register_container.ResumeLayout(false);
-            register_container.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private MetroFramework.Controls.MetroButton login_button;
-        private MetroFramework.Controls.MetroTextBox username;
-        private MetroFramework.Controls.MetroTextBox password;
-        private MetroFramework.Controls.MetroButton register_button;
+        private MetroFramework.Controls.MetroButton submit_button;
+        private MetroFramework.Controls.MetroButton cancel_button;
+        private MetroFramework.Controls.MetroComboBox admin_values;
+        private MetroFramework.Controls.MetroLabel info_label;
     }
 }
--- Assessment2/Solution/Views/Dialogues/AdministrationDialogue.cs
+++ Assessment2/Solution/Views/Dialogues/AdministrationDialogue.cs
@@ -3,29 +3,26 @@ using System.Collections.Generic;
 using System.Windows.Forms;
 using Assessment2.Solution.Users;
 using Assessment2.Solution.Users.Abs;
+using Assessment2.Solution.Users.Impl;
+using System.Collections.ObjectModel;
 
 namespace Assessment2.Solution.Views.Dialogues {
 
     public partial class AdministrationDialogue : MetroFramework.Forms.MetroForm {
 
         private readonly UserHandler _handler;
-        private readonly List<User> _users;
+        private readonly ReadOnlyCollection<User> _users;
 
         public AdministrationDialogue(UserHandler handler, List<User> users) {
             _handler = handler;
-            _users = users;
+            _users = new ReadOnlyCollection<User>(users);
             InitializeComponent();
-            login_button.Enabled = false;
+
+            admin_values.DataSource = Enum.GetNames(typeof(Admin.AdminType));
+            info_label.Text = $@"You are modifying the rank of {users.Count} user(s).";
         }
 
         private void login_button_Click(object sender, EventArgs e) {
-            if (!_handler.Login(username.Text, password.Text)) {
-                MessageBox.Show(@"Incorrect username or password.");
-                return;
-            }
-            
-            password.Text = string.Empty;
-
             var users = new UserListScreen(this, _handler);
 
             users.Show();
@@ -42,7 +39,6 @@ namespace Assessment2.Solution.Views.Dialogues {
 
             Hide();
         }
-
     }
 
 }
